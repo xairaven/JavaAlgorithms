eb8a4b01b8384adf7e1e8930222a517a59b9f9e6
xairaven <alexkovalevkpi@gmail.com>
2022-01-25 23:03:18 +0200

    refactor: added binary-search method

--- Algorithms/Search/Binary-Search-Java/src/xairaven/Main.java
+++ Algorithms/Search/Binary-Search-Java/src/xairaven/Main.java
@@ -1,7 +1,24 @@
 package xairaven;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int key = 688426; //number from largeWSorted.txt
+        Scanner in = new Scanner("../resources/largeWSorted.txt");
+        while(in.hasNextInt()) {
+            System.out.println(in.nextInt());
+        }
+    }
+
+    public static int binarySearch(int key, int[] arr, int lo, int hi) {
+        if (arr == null) throw new IllegalArgumentException();
+        if (lo <= hi) {
+            int middle = lo + (hi - lo) / 2;
+            if (key < arr[middle]) return binarySearch(key, arr, lo, middle - 1);
+            else if (key > arr[middle]) return binarySearch(key, arr, middle + 1, hi);
+            else return middle;
+        } else {
+            return -1;
+        }
     }
 }
