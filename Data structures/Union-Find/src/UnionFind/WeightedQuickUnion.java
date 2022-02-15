05eab0e172d3060440765f402cac8c547b69a3bc
xairaven <alexkovalevkpi@gmail.com>
2022-02-15 21:06:43 +0200

    fix: fixed sz[]

--- Data structures/Union-Find/src/UnionFind/WeightedQuickUnion.java
+++ Data structures/Union-Find/src/UnionFind/WeightedQuickUnion.java
@@ -8,9 +8,11 @@ public class WeightedQuickUnion {
     public WeightedQuickUnion(int N) {
         count = N;
         id = new int[N];
-        for (int i = 0; i < id.length; i++) id[i] = i;
         sz = new int[N];
-        for (int i = 0; i < sz.length; i++) sz[i] = i;
+        for (int i = 0; i < N; i++)  {
+            id[i] = i;
+            sz[i] = 1;
+        }
     }
 
     public boolean connected(int p, int q) {
